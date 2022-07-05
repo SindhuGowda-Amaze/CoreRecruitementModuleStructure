8efae7dcf5df235bfb6e098114242164029ac300
ManikantaSwamy <ManikantaSwamy117@gmail.com>
2022-07-05 14:44:26 +0530

    ss

--- src/app/Modules/shared/shared.module.ts
+++ src/app/Modules/shared/shared.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
+import { HelpComponent } from './help/help.component';
+import { SupportTicketDashboardComponent } from './support-ticket-dashboard/support-ticket-dashboard.component';
+import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
 
 
 @NgModule({
   declarations: [
-    SharedComponent
+    SharedComponent,
+    HelpComponent,
+    SupportTicketDashboardComponent,
+    SupportTicketsComponent
   ],
   imports: [
     CommonModule,
