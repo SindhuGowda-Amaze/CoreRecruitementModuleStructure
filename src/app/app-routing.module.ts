3c7af5ed41c5e41d77077206129cc8fc149634a0
ManikantaSwamy <ManikantaSwamy117@gmail.com>
2022-07-12 10:23:46 +0530

    ss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'admin', loadChildren: () => import('./Modules/admin/admin.module').then(m => m.AdminModule) },
   { path: 'recruiter', loadChildren: () => import('./Modules/recruiter/recruiter.module').then(m => m.RecruiterModule) },
   { path: 'vendor', loadChildren: () => import('./Modules/vendor/vendor.module').then(m => m.VendorModule) },
-  { path: 'hiringmanager', loadChildren: () => import('./Modules/Client/hiringmanager/hiringmanager.module').then(m => m.HiringmanagerModule) },
+  { path: 'hirignmanager', loadChildren: () => import('./Modules/Client/hiringmanager/hiringmanager.module').then(m => m.HiringmanagerModule) },
   { path: 'shared', loadChildren: () => import('./Modules/shared/shared.module').then(m => m.SharedModule) },
   { path: 'InterviewPanel', loadChildren: () => import('./Modules/Client/interview-panel/interview-panel.module').then(m => m.InterviewPanelModule) },
   { path: 'BUHead', loadChildren: () => import('./Modules/Client/buhead/buhead.module').then(m => m.BUHeadModule) },
