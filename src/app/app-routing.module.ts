c7447c78bf396eb1fb9eb9b0ef9655150ec6bcd1
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2022-07-15 11:25:14 +0530

    ss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: 'vendor', loadChildren: () => import('./Modules/vendor/vendor.module').then(m => m.VendorModule) },
   { path: 'hirignmanager', loadChildren: () => import('./Modules/Client/hiringmanager/hiringmanager.module').then(m => m.HiringmanagerModule) },
   { path: 'shared', loadChildren: () => import('./Modules/shared/shared.module').then(m => m.SharedModule) },
-  { path: 'n', loadChildren: () => import('./Modules/Client/interview-panel/interview-panel.module').then(m => m.InterviewPanelModule) },
+  { path: 'InterviewPanel', loadChildren: () => import('./Modules/Client/interview-panel/interview-panel.module').then(m => m.InterviewPanelModule) },
   { path: 'BUHead', loadChildren: () => import('./Modules/Client/buhead/buhead.module').then(m => m.BUHeadModule) },
   { path: 'manager', loadChildren: () => import('./Modules/Client/manager/manager.module').then(m => m.ManagerModule) },
   { path: 'report', loadChildren: () => import('./Modules/report/report.module').then(m => m.ReportModule) },
