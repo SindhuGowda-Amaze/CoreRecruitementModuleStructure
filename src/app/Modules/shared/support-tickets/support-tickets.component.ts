999324da658929232d165d71bf7d8bd045c6371f
Mamata Das <mamata@getmybubble.com>
2022-08-08 15:18:56 +0530

    jjj

--- src/app/Modules/shared/support-tickets/support-tickets.component.ts
+++ src/app/Modules/shared/support-tickets/support-tickets.component.ts
@@ -115,7 +115,7 @@ export class SupportTicketsComponent implements OnInit {
         this.files.length = 0;
       },
       error: (err: { error: { message: any } }) => {
-        Swal.fire(' Getting Attachments ');
+        Swal.fire('Attachment Upload Successfully ');
         // Insert error in Db Here//
         var obj = {
           PageName: this.currentUrl,
